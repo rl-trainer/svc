cab6ffa7669fb9f69d7c0ef0e68fcd5f1546cc48
notversed <leestephens2004@icloud.com>
2026-08-07 17:18:51 -0500

    Initial commit

---
+++ src/main/java/com/versed/rl_trainer_svc/RlTrainerSvcApplication.java
@@ -0,0 +1,13 @@
+package com.versed.rl_trainer_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlTrainerSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlTrainerSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/versed/rl_trainer_svc/RlTrainerSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.versed.rl_trainer_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlTrainerSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
